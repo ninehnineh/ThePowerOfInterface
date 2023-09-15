9cdcceb6c428f12b95f1f0d039464635ecc5a5d3
ninehnineh <103179810+ninehnineh@users.noreply.github.com>
2023-09-15 16:40:24 +0700

    Add project files.

---
+++ DemoLibrary/PhysicalProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class PhysicalProductModel
+    {
+        public string Title { get; set; }
+
+        public void ShipItem(CustomerModel customer)
+        {
+            Console.WriteLine($"Simulating shipping { Title } to { customer.FirstName }{ customer.LastName } in { customer.City }");
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,43 @@
+﻿using DemoLibrary;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PhysicalProductModel> cart = AddSampleData();
+            CustomerModel customer = GetCustomer();
+
+            foreach (PhysicalProductModel prod in cart)
+            {
+                prod.ShipItem(customer);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static CustomerModel GetCustomer()
+        {
+            return new CustomerModel
+            {
+                FirstName = "Truong",
+                LastName = "Chinh",
+                City = "HoChiMinh",
+                EmailAddress = "chinh@gmeol.com",
+                PhoneNumber = "0353555555"
+            };
+        }
+
+        private static List<PhysicalProductModel> AddSampleData()
+        {
+            List<PhysicalProductModel> output = new List<PhysicalProductModel>();
+
+            output.Add(new PhysicalProductModel { Title = "Sting" });
+            output.Add(new PhysicalProductModel { Title = "RebBull" });
+            output.Add(new PhysicalProductModel { Title = "NiveaMen" });
+
+            return output;
+        }
+    }
+}
